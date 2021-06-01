e05f18ee94b0ad3dbfb291d73818dfd22a6d66ea
echyassine <yassinekaist97@gmail.com>
2021-06-01 16:14:09 +0100

    Tp1 SERVERLESS ET CLOUD NATIVE APPLICATIONS

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCXAG-FqXO2bhc1kgZ5nhxcfs4cUtKuBuU",
+    authDomain: "cours-tp-82a3e.firebaseapp.com",
+    projectId: "cours-tp-82a3e",
+    storageBucket: "cours-tp-82a3e.appspot.com",
+    messagingSenderId: "1090938335310",
+    appId: "1:1090938335310:web:ef883a9036b846a8b6931f"
+  }
 };
 
 /*
