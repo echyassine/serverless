bb2e58c3ffb6d4e8fa300cbd7d7d72f46168956b
echyassine <yassinekaist97@gmail.com>
2021-06-01 17:29:30 +0100

    modify some parameter to deploy the app

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,7 +1,7 @@
  
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { from, Observable } from 'rxjs';
+import {  Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCXAG-FqXO2bhc1kgZ5nhxcfs4cUtKuBuU",
+    authDomain: "cours-tp-82a3e.firebaseapp.com",
+    projectId: "cours-tp-82a3e",
+    storageBucket: "cours-tp-82a3e.appspot.com",
+    messagingSenderId: "1090938335310",
+    appId: "1:1090938335310:web:ef883a9036b846a8b6931f"
+  }
 };
